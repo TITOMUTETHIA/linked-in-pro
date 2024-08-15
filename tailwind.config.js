976b9bc9ceb19976f7466e75f5619de4b062de8d
Tito Mutethia <titomutethia@gmail.com>
2024-08-15 17:45:45 +0300

    third comitt

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,12 @@ export default {
 
     theme: {
         extend: {
+            colors: {
+                "black": "#060606",
+                'primary': '#3490dc',
+                'secondary': '#ffed4a',
+                'danger': '#e3342f',
+            },
             
         },
     },
